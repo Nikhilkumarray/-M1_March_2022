8875187e9b79f8a4478bb7d20d8692e301f6b120
nikhil kumar ray <nikhilray023@gmail.com>
2022-04-02 23:04:34 +0530

    commit

---
+++ 3_Implementation/inc/function.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTION_H
+#define FUNCTION_H
+void login();
+void Menu();
+void checkBalance(float balance);
+float Deposit(float balance);
+float moneyWithdraw(float balance);
+void menuExit();
+void errorMessage();
+#endif
